528b0a57464bfafa3fbe4e901ce81312a83fdce8
joegruss <joe.gruss@lifecanvastech.com>
2022-06-03 18:46:44 -0400

    added test message for jenny

--- pystripe/core.py
+++ pystripe/core.py
@@ -19,6 +19,7 @@ warnings.filterwarnings("ignore")
 supported_extensions = ['.tif', '.tiff', '.raw', '.dcimg', '.png']
 nb_retry = 10
 
+print('test')
 
 def _get_extension(path):
     """Extract the file extension from the provided path
